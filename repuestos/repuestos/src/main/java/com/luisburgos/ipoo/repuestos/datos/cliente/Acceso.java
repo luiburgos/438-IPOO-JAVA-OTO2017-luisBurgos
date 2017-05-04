da22a5d99d476e471dc829372ba08c23bc8d3853
lburgos <lburgos@ipar.com.py>
2017-05-03 21:44:51 -0400

    Agregada la Clase Acceso

---
+++ repuestos/repuestos/src/main/java/com/luisburgos/ipoo/repuestos/datos/cliente/Acceso.java
@@ -0,0 +1,18 @@
+package com.luisburgos.ipoo.repuestos.datos.cliente;
+
+import java.util.Date;
+
+public class Acceso {
+
+		private Date fechaHoraInicio;
+		private Date fechaHoraFin;
+		private String ip;
+		private Cliente cliente;
+		
+		
+		
+		public Acceso (){
+			
+		}
+		
+}
