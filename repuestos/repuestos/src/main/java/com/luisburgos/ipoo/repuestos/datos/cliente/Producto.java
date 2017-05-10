d292e47cc19d577ad1e56b10464d807c7f84a173
alumno-202 <alumno-202@M202-16.uaa.local>
2017-05-10 19:48:56 -0400

    crear clase producto

---
+++ repuestos/repuestos/src/main/java/com/luisburgos/ipoo/repuestos/datos/cliente/Producto.java
@@ -0,0 +1,24 @@
+package com.luisburgos.ipoo.repuestos.datos.cliente;
+
+
+//Inicio de la clase
+//nomenclatura: visibilidad class nombre de la clase
+public class Producto {
+
+	//atributos
+	private int id;
+	private String nombre;
+	private String descripcion;
+	private String imagen;
+	private Double precio;
+	
+	//constructor 1
+		public Producto () {
+			
+		}		
+	
+	//constructor 2
+		
+	
+	
+}
