b4adfe913d0f459d594659f870c0b92e564f0b03
alumno-202 <alumno-202@M202-16.uaa.local>
2017-05-10 19:55:28 -0400

    clase prodcucto

--- repuestos/repuestos/src/main/java/com/luisburgos/ipoo/repuestos/datos/cliente/Producto.java
+++ repuestos/repuestos/src/main/java/com/luisburgos/ipoo/repuestos/datos/cliente/Producto.java
@@ -26,10 +26,55 @@ public class Producto {
 		this.descripcion = descripcion;
 		this.imagen = imagen;
 		this.precio = precio;
-	}		
-	
-		//metodo
+	}
+
 		
+		//metodo (source - generate getters and setters - seleccionar last member
+		
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		public String getNombre() {
+			return nombre;
+		}
+
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+
+		public String getDescripcion() {
+			return descripcion;
+		}
+
+		public void setDescripcion(String descripcion) {
+			this.descripcion = descripcion;
+		}
+
+		public String getImagen() {
+			return imagen;
+		}
+
+		public void setImagen(String imagen) {
+			this.imagen = imagen;
+		}
+
+		public Double getPrecio() {
+			return precio;
+		}
+
+		public void setPrecio(Double precio) {
+			this.precio = precio;
+		}		
+	
+		
+		
+		
+		// fin de la clase
 		
 	
 	
