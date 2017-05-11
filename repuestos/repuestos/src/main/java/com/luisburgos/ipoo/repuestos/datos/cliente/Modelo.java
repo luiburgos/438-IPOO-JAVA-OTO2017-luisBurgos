bccf26321faa359e231ae563512337d1c08dfa01
alumno-202 <alumno-202@M202-16.uaa.local>
2017-05-10 21:23:49 -0400

    clase modelo

--- repuestos/repuestos/src/main/java/com/luisburgos/ipoo/repuestos/datos/cliente/Modelo.java
+++ repuestos/repuestos/src/main/java/com/luisburgos/ipoo/repuestos/datos/cliente/Modelo.java
@@ -2,4 +2,14 @@ package com.luisburgos.ipoo.repuestos.datos.cliente;
 
 public class Modelo {
 
+	
+	private int id;
+	private String nombre;
+	private String descripcion;
+	
+	
+	public Modelo () {
+		
+	}
+
 }
