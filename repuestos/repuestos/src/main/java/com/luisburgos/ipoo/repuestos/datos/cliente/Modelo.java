ccfd122a1d40962ca08c950abd1bdabce72e27d3
alumno-202 <alumno-202@M202-16.uaa.local>
2017-05-10 21:25:06 -0400

    modelo

--- repuestos/repuestos/src/main/java/com/luisburgos/ipoo/repuestos/datos/cliente/Modelo.java
+++ repuestos/repuestos/src/main/java/com/luisburgos/ipoo/repuestos/datos/cliente/Modelo.java
@@ -1,15 +1,57 @@
 package com.luisburgos.ipoo.repuestos.datos.cliente;
+//Inicio de la Clase
 
 public class Modelo {
 
-	
+	//atributos
 	private int id;
 	private String nombre;
 	private String descripcion;
 	
-	
+	//constructor 1
 	public Modelo () {
 		
 	}
+
+	//constructor 2
+	public Modelo(int id, String nombre, String descripcion) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+	}
+
+	//constructor 2
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+	
+	
 
 }
